724be3d0bd272f0070e099ddc1a2bb61f91da580
NumanBashir <sportybashir@gmail.com>
2024-02-28 09:55:27 +0100

    added postgres

---
+++ backend/database.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE insuranceservice;
+
+CREATE TABLE service(
+    service_id SERIAL PRIMARY KEY, 
+    description VARCHAR(255)
+)
--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,27 @@
 import express from "express";
 import { PORT } from "./config.js ";
 import cors from "cors";
+import pool from "./db.js";
 
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 
 app.get("/", (request, response) => {
   console.log(request);
   return response.status(234).send("Welcome to InsuranceService!");
+});
+
+app.get("/services", async (req, res) => {
+  try {
+    const allServices = await pool.query("SELECT * FROM service");
+    console.log(allServices.rows); // Log the data to the console
+    res.json(allServices.rows); // Send the data as JSON to the client
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
 });
 
 app.listen(PORT, () => {
