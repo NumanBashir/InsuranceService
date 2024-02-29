d83b4b777bcd76a1b78dcbecee9818cdde5b637a
NumanBashir <sportybashir@gmail.com>
2024-02-29 11:24:14 +0100

    remove pg

--- backend/database.sql
+++
@@ -1,6 +0,0 @@
-CREATE DATABASE insuranceservice;
-
-CREATE TABLE service(
-    service_id SERIAL PRIMARY KEY, 
-    description VARCHAR(255)
-)
--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,6 @@
 import express from "express";
 import { PORT } from "./config.js ";
 import cors from "cors";
-import pool from "./db.js";
 
 const app = express();
 
@@ -11,17 +10,6 @@ app.use(express.json());
 app.get("/", (request, response) => {
   console.log(request);
   return response.status(234).send("Welcome to InsuranceService!");
-});
-
-app.get("/services", async (req, res) => {
-  try {
-    const allServices = await pool.query("SELECT * FROM service");
-    console.log(allServices.rows); // Log the data to the console
-    res.json(allServices.rows); // Send the data as JSON to the client
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send("Server error");
-  }
 });
 
 app.listen(PORT, () => {
