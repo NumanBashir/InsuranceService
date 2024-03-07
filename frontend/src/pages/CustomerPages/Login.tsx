b96a616336e5af8fee3c6ee4aa94652b7d727703
NumanBashir <sportybashir@gmail.com>
2024-03-07 20:02:42 +0100

    add spinner

--- frontend/src/pages/CustomerPages/Login.tsx
+++ frontend/src/pages/CustomerPages/Login.tsx
@@ -1,11 +1,14 @@
 import axios from "axios";
 import NameCard from "../../components/NameCard";
 import React, { useState, useEffect } from "react";
+import Spinner from "../../components/Spinner";
 
 const Login = () => {
   const [userNames, setUserNames] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
+    setLoading(true);
     axios
       .get("http://localhost:3000/users")
       .then((response) => {
@@ -13,29 +16,35 @@ const Login = () => {
           (user: { name: any }) => user.name
         );
         setUserNames(names);
+        setLoading(false);
       })
       .catch((error) => {
         console.error("Error fetching users:", error);
+        setLoading(false);
       });
   }, []);
 
   return (
     <>
-      <div className="flex justify-center items-center">
-        <div className="bg-white shadow-lg rounded-2xl py-8 m-4 w-1/2 max-w-2xl">
-          <div className="flex flex-col items-center mb-4">
-            <span className="text-xl font-semibold">Customer Login:</span>
-            {userNames.map((name, index) => (
-              <NameCard key={index} name={name} />
-            ))}
-          </div>
-          <div className="flex flex-col items-center">
-            <span className="text-xl font-semibold">Admin Login:</span>
-            <NameCard name="Allan Admin" />
-            {/* This will be changed to AdminCard Login */}
+      {loading ? (
+        <Spinner />
+      ) : (
+        <div className="flex justify-center items-center">
+          <div className="bg-white shadow-lg rounded-2xl py-8 m-4 w-1/2 max-w-2xl">
+            <div className="flex flex-col items-center mb-4">
+              <span className="text-xl font-semibold">Customer Login:</span>
+              {userNames.map((name, index) => (
+                <NameCard key={index} name={name} />
+              ))}
+            </div>
+            <div className="flex flex-col items-center">
+              <span className="text-xl font-semibold">Admin Login:</span>
+              <NameCard name="Allan Admin" />
+              {/* This will be changed to AdminCard Login */}
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </>
   );
 };
