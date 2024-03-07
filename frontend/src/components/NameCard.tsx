57b72e760e88281c717d3142b4612368dd1b229e
NumanBashir <sportybashir@gmail.com>
2024-03-07 19:24:49 +0100

    fetch users name to login

--- frontend/src/components/NameCard.tsx
+++ frontend/src/components/NameCard.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const NameCard = () => {
+interface Props {
+  name: string;
+}
+
+const NameCard: React.FC<Props> = ({ name }) => {
   return (
     <>
       <Link
         to="/home"
         className="inline-flex justify-center items-center text-white drop-shadow-md bg-tertiary rounded-md w-1/4 m-1 p-1"
       >
-        <button>Navn Navnsen</button>
+        <button>{name}</button>
       </Link>
     </>
   );
--- frontend/src/pages/CustomerPages/Login.tsx
+++ frontend/src/pages/CustomerPages/Login.tsx
@@ -1,17 +1,38 @@
+import axios from "axios";
 import NameCard from "../../components/NameCard";
+import React, { useState, useEffect } from "react";
 
 const Login = () => {
+  const [userNames, setUserNames] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/users")
+      .then((response) => {
+        const names = response.data.data.map(
+          (user: { name: any }) => user.name
+        );
+        setUserNames(names);
+      })
+      .catch((error) => {
+        console.error("Error fetching users:", error);
+      });
+  }, []);
+
   return (
     <>
       <div className="flex justify-center items-center">
         <div className="bg-white shadow-lg rounded-2xl py-8 m-4 w-1/2 max-w-2xl">
+          <div className="flex flex-col items-center mb-4">
+            <span className="text-xl font-semibold">Customer Login:</span>
+            {userNames.map((name, index) => (
+              <NameCard key={index} name={name} />
+            ))}
+          </div>
           <div className="flex flex-col items-center">
             <span className="text-xl font-semibold">Admin Login:</span>
-            <NameCard />
-            <span className="text-xl font-semibold mt-8">Customer Login:</span>
-            <NameCard />
-            <NameCard />
-            <NameCard />
+            <NameCard name="Allan Admin" />
+            {/* This will be changed to AdminCard Login */}
           </div>
         </div>
       </div>
