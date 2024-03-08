421c36a3a21e819a0a833f00c76b4ad4def00215
alecranjitkar <alexranjitkar@gmail.com>
2024-03-08 15:45:20 +0100

    updates on CustomerHome

--- frontend/src/components/NameCard.tsx
+++ frontend/src/components/NameCard.tsx
@@ -1,23 +1,25 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 interface Props {
   name: string;
 }
 
 const NameCard: React.FC<Props> = ({ name }) => {
+  const navigate = useNavigate();
+
+  const handleNavigate = () => {
+    navigate("/home", { state: { name } });
+  };
+
   return (
-    <>
-      <Link
-        to="/home"
-        className="inline-flex justify-center items-center text-white drop-shadow-md bg-tertiary rounded-md w-1/4 m-1 p-1"
-      >
-        <button>{name}</button>
-      </Link>
-    </>
+    <div
+      className="inline-flex justify-center items-center text-white drop-shadow-md bg-tertiary rounded-md w-1/4 m-1 p-1"
+      onClick={handleNavigate}
+    >
+      <button>{name}</button>
+    </div>
   );
 };
 
 export default NameCard;
-
-// TODO: props for passing name of each User
--- frontend/src/pages/CustomerPages/CustomerHome.tsx
+++ frontend/src/pages/CustomerPages/CustomerHome.tsx
@@ -1,5 +1,17 @@
+// CustomerHome.tsx
+import { useLocation } from "react-router-dom";
+
 const CustomerHome = () => {
-  return <div>CustomerHome</div>;
+  const location = useLocation();
+  const state = location.state as { name: string };
+
+  return (
+    <div className="absolute top-0 left-0 right-0 h-72 flex justify-center items-center">
+      <span className="font-bold text-white">
+        Velkommen til Insurance service, {state.name}
+      </span>
+    </div>
+  );
 };
 
 export default CustomerHome;
