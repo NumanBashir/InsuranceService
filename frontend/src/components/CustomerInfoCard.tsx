c1e022adcea216dd20e66c6139d560da5ed221c2
NumanBashir <sportybashir@gmail.com>
2024-03-15 15:33:25 +0100

    Use own components to display each user

--- frontend/src/components/CustomerInfoCard.tsx
+++ frontend/src/components/CustomerInfoCard.tsx
@@ -1,15 +1,29 @@
 import React from "react";
 
-const CustomerInfoCard = () => {
+interface Props {
+  name: string;
+  address: string;
+  email: string;
+}
+
+const CustomerInfoCard: React.FC<Props> = ({
+  name,
+  address,
+  email,
+  // onButtonClick,
+}) => {
   return (
     <div className="flex justify-center items-center my-6">
-      <div className="bg-userColor rounded-xl p-4 flex justify-between items-center w-1/2">
+      <div className="bg-userColor rounded-xl p-4 flex justify-between items-center w-full md:w-1/2">
         <div>
-          <div className="text-lg font-bold">Reece James</div>
-          <div className="text-gray-600">Adresse 12, 2750 Ballerup</div>
-          <div className="text-gray-600">reece@chelsea.com</div>
+          <div className="text-lg font-bold">{name}</div>
+          <div className="text-gray-600">{address}</div>
+          <div className="text-gray-600">{email}</div>
         </div>
-        <button className="text-tertiary font-bold px-4">
+        <button
+          className="text-tertiary font-bold px-4 py-2 rounded-lg hover:bg-gray-200 transition duration-300"
+          // onClick={onButtonClick}
+        >
           Se mere &gt;&gt;
         </button>
       </div>
--- frontend/src/pages/AdminPages/AdminHome.tsx
+++ frontend/src/pages/AdminPages/AdminHome.tsx
@@ -63,40 +63,30 @@ const AdminHome = () => {
               type="search"
               id="default-search"
               className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="Søg på en kundes ID, navn eller email"
+              placeholder="Søg på kundens navn eller email"
               required
             />
             <button
               type="submit"
-              className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              className="text-white absolute end-2.5 bottom-2.5 bg-tertiary hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
               <HiMagnifyingGlass />
             </button>
           </div>
         </form>
-        <CustomerInfoCard />
-        <div className="flex justify-center items-center">
-          <div className="bg-white shadow-lg rounded-2xl py-8 m-4 w-1/2 max-w-2xl">
-            {searchResults.length > 0 ? (
-              searchResults.map((user) => (
-                <div key={user._id} className="flex flex-col items-center mb-4">
-                  <span className="text-lg font-semibold">{user.name}</span>
-                  {user.email && (
-                    <span className="text-sm text-gray-600">{user.email}</span>
-                  )}
-                  {user.address && (
-                    <span className="text-sm text-gray-600">
-                      {user.address}
-                    </span>
-                  )}
-                </div>
-              ))
-            ) : (
-              <div className="text-center text-gray-500">
-                Ingen resultater fundet
-              </div>
-            )}
-          </div>
+        <span className="font-bold text-black text-2xl flex justify-center my-10">
+          Vælg hvilken kunde du ønsker at se
+        </span>
+        <div>
+          {searchResults.map((user) => (
+            <CustomerInfoCard
+              key={user._id}
+              name={user.name}
+              address={user.address || "Adresse ikke tilgængelig"}
+              email={user.email || "Email ikke tilgængelig"}
+              // onButtonClick={() => {/* handle the button click event */}}
+            />
+          ))}
         </div>
       </div>
     </>
