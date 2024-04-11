ffaf49d57c56977d9913deb1e66639a2605c6b94
NumanBashir <sportybashir@gmail.com>
2024-04-11 16:51:37 +0200

    dotenv instead of config

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,15 @@
 import express, { response } from "express";
-import { PORT, mongoDBURL } from "./config.js ";
 import cors from "cors";
 import mongoose from "mongoose";
 import insurancesRoutes from "./routes/insurancesRoutes.js";
 import servicesRoutes from "./routes/servicesRoutes.js";
 import usersRoutes from "./routes/usersRoutes.js";
 import ordersRoutes from "./routes/ordersRoutes.js";
+import "dotenv/config";
 
 const app = express();
+const PORT = process.env.PORT;
+const mongoDBURL = process.env.mongoDBURL;
 
 app.use(cors());
 app.use(express.json());
