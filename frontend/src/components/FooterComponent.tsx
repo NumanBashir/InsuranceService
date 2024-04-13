e366f71ddd408567ffa5e1d2372f324846481796
NumanBashir <sportybashir@gmail.com>
2024-04-13 11:10:05 +0200

    Finish up proper links and texts

--- frontend/src/components/FooterComponent.tsx
+++ frontend/src/components/FooterComponent.tsx
@@ -1,45 +1,65 @@
 import { Footer } from "flowbite-react";
-import { BsFacebook, BsGithub, BsInstagram, BsTwitter } from "react-icons/bs";
+import { BsFacebook, BsLinkedin, BsHandThumbsUp } from "react-icons/bs";
+import { SiTrustpilot } from "react-icons/si";
 
 const FooterComponent = () => {
   return (
     <>
       <Footer className="mt-[500px] bg-primary rounded-none ">
         <div className="w-full">
-          <div className="grid w-full grid-cols-2 gap-8 px-6 py-8 md:grid-cols-4 ">
+          <div className="grid w-full grid-cols-2 gap-8 px-6 py-8 md:grid-cols-3">
             <div>
               <Footer.Title title="Om Topdanmark" className="text-white" />
               <Footer.LinkGroup col className="text-secondary">
-                <Footer.Link href="#">Om Topdanmark</Footer.Link>
-                <Footer.Link href="#">Job og Karriere</Footer.Link>
-                <Footer.Link href="#">Investor relations</Footer.Link>
-                <Footer.Link href="#">Sådan arbejder vi</Footer.Link>
+                <Footer.Link href="https://www.topdanmark.dk/om-topdanmark/">
+                  Om Topdanmark
+                </Footer.Link>
+                <Footer.Link href="https://www.topdanmark.dk/om-topdanmark/job-og-karriere/">
+                  Job og Karriere
+                </Footer.Link>
+                <Footer.Link href="https://www.topdanmark.dk/om-topdanmark/investor-relations/">
+                  Investor relations
+                </Footer.Link>
+                <Footer.Link href="https://www.topdanmark.dk/saadan-arbejder-vi/">
+                  Sådan arbejder vi
+                </Footer.Link>
               </Footer.LinkGroup>
             </div>
             <div>
-              <Footer.Title title="help center" className="text-white" />
+              <Footer.Title title="Kundeservice" className="text-white" />
               <Footer.LinkGroup col className="text-secondary">
-                <Footer.Link href="#">Discord Server</Footer.Link>
-                <Footer.Link href="#">Twitter</Footer.Link>
-                <Footer.Link href="#">Facebook</Footer.Link>
-                <Footer.Link href="#">Contact Us</Footer.Link>
+                <Footer.Link href="https://www.topdanmark.dk/faa-hjaelp/kontakt-os/">
+                  Kontakt os
+                </Footer.Link>
+                <Footer.Link href="https://www.topdanmark.dk/fordele/">
+                  Dine fordele hos os
+                </Footer.Link>
+                <Footer.Link href="https://www.topdanmark.dk/faa-hjaelp/">
+                  Spørgsmål og svar
+                </Footer.Link>
+                <Footer.Link href="https://www.topdanmark.dk/vilkaar/">
+                  Find vilkår
+                </Footer.Link>
               </Footer.LinkGroup>
             </div>
             <div>
-              <Footer.Title title="legal" className="text-white" />
+              <Footer.Title title="Forsikringer" className="text-white" />
               <Footer.LinkGroup col className="text-secondary">
-                <Footer.Link href="#">Privacy Policy</Footer.Link>
-                <Footer.Link href="#">Licensing</Footer.Link>
-                <Footer.Link href="#">Terms &amp; Conditions</Footer.Link>
-              </Footer.LinkGroup>
-            </div>
-            <div>
-              <Footer.Title title="download" className="text-white" />
-              <Footer.LinkGroup col className="text-secondary">
-                <Footer.Link href="#">iOS</Footer.Link>
-                <Footer.Link href="#">Android</Footer.Link>
-                <Footer.Link href="#">Windows</Footer.Link>
-                <Footer.Link href="#">MacOS</Footer.Link>
+                <Footer.Link href="https://www.topdanmark.dk/forsikringer/">
+                  Alle forsikringer
+                </Footer.Link>
+                <Footer.Link href="https://www.topdanmark.dk/forsikringer/bilforsikring/">
+                  Bilforsikring
+                </Footer.Link>
+                <Footer.Link href="https://www.topdanmark.dk/forsikringer/husforsikring/">
+                  Husforsikring
+                </Footer.Link>
+                <Footer.Link href="https://www.topdanmark.dk/forsikringer/indboforsikring/">
+                  Indboforsikring
+                </Footer.Link>
+                <Footer.Link href="https://www.topdanmark.dk/forsikringer/ulykkesforsikring/">
+                  Ulykkeforsikring
+                </Footer.Link>
               </Footer.LinkGroup>
             </div>
           </div>
@@ -51,10 +71,26 @@ const FooterComponent = () => {
               className="text-white"
             />
             <div className="mt-4 flex space-x-6 sm:mt-0 sm:justify-center">
-              <Footer.Icon href="#" icon={BsFacebook} className="text-white" />
-              <Footer.Icon href="#" icon={BsInstagram} className="text-white" />
-              <Footer.Icon href="#" icon={BsTwitter} className="text-white" />
-              <Footer.Icon href="#" icon={BsGithub} className="text-white" />
+              <Footer.Icon
+                href="https://www.facebook.com/Topdanmark"
+                icon={BsFacebook}
+                className="text-white"
+              />
+              <Footer.Icon
+                href="https://www.linkedin.com/company/topdanmark"
+                icon={BsLinkedin}
+                className="text-white"
+              />
+              <Footer.Icon
+                href="https://dk.trustpilot.com/review/www.topdanmark.dk"
+                icon={SiTrustpilot}
+                className="text-white"
+              />
+              <Footer.Icon
+                href="https://www.topdanmark.dk/blog/"
+                icon={BsHandThumbsUp}
+                className="text-white"
+              />
             </div>
           </div>
         </div>
