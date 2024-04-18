80c437fb54c251c940c98c61a8e88da77fd6e7fd
NumanBashir <sportybashir@gmail.com>
2024-04-18 10:33:13 +0200

    update order model to take otherInfo

--- frontend/src/pages/CustomerPages/Billing.tsx
+++ frontend/src/pages/CustomerPages/Billing.tsx
@@ -158,7 +158,7 @@ const Billing: React.FC = () => {
         </div>
         <button
           type="submit"
-          className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          className="mt-4 bg-tertiary hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
         >
           Submit
         </button>
